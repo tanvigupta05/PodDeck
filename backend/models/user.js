40cebc7603d6f145b55accbba84a9021e97af98c
Tanvi Gupta <tanvi05@gmail.com>
2024-11-11 23:45:21 +0530

    Schemas created for user, podcast and category

---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const user = new mongoose.Schema({
+    username:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    podcasts:[
+        {
+            type: mongoose.Types.ObjectId,
+            ref: "podcasts"  // variable name given to podcast schema
+        },
+    ],
+},
+{timestamps:true});
+
+module.exports = mongoose.model("user",user);
