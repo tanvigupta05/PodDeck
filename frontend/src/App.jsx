38c2a6d6ec8d846b8e9cd6fd2731b830001cd00d
Tanvi Gupta <tanvi05@gmail.com>
2024-11-21 10:35:44 +0530

    created categories page where all genres are displayed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import MainLayout from './layout/MainLayout';
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import AuthLayout from "./layout/AuthLayout";
-import Login from "./pages/Login.jsx";
+import Login from "./pages/Login";
+import Categories from "./pages/Categories";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<MainLayout/>}>
             {" "}
             <Route index element={<Home/>} />
+            <Route path="/categories" element={<Categories/>}/>
           </Route>
           <Route path="/" element={<AuthLayout/>}>
             <Route path="/signup" element={<Signup/>} />
